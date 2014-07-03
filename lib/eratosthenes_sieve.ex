f3c5265de27a591c002120f8a8a8e12fea4b3fe3
Stratus3D <admin@stratus3d.com>
2014-07-02 20:58:53 -0400

    Completed the algorithm. Just need to write tests and configure the app.

--- lib/eratosthenes_sieve.ex
+++ lib/eratosthenes_sieve.ex
@@ -17,16 +17,21 @@ defmodule EratosthenesSieve do
     # Let p equal 2, the first prime number we start with
     p = 2
 
-    # if a number in the range of possible primes is a
+    # We now compute the prime numbers in the range of
+    # possible_values by removing the numbers we know are
+    # not prime. To determine which numbers are not prime
+    # we simply check to if they are a mulitple of a
+    # prime and also greater than the prime.
+    remove_multiples_of_primes(p, possible_values)
+  end
+
+  def remove_multiples_of_primes(p, possible_values) do
+    # If a number in the range of possible primes is a
     # multiple of p and greater than two, remove it. We
     # then reassign p to the next highest value in the
     # new list, which is a prime. We then repeat removal
     # of values that are multiples of and greater than
     # its value
-    remove_multiples_of_primes(p, possible_values)
-  end
-
-  def remove_multiples_of_primes(p, possible_values) do
     new_values = remove_multiples_of(p, possible_values)
     case find_next_prime(p, new_values) do
       {:error, :no_prime} ->
@@ -36,11 +41,22 @@ defmodule EratosthenesSieve do
     end
   end
 
-  def find_next_prime(p, new_values) do
-    # not implemented
+  def find_next_prime(p, values) do
+    # Find the next prime number in the list. If a number
+    # is greater than p return it. If we are at the end 
+    # of the list, no number will be greater than p, in
+    # which case we return {:error, :no_prime}.
+    case :lists.dropwhile(fn(value) ->
+      value > p
+      end, values) do
+      [] ->
+        {:error, :no_prime}
+      [value|_] ->
+        value
+    end
   end
 
-  # remove multiples of p that are greater than p
+  # Remove multiples of p that are greater than p
   def remove_multiples_of(p, list) do
     lc value inlist list, 0 == rem(value, p), do: value
   end
